48fd2ee090d5d4c3300001e49b9a4f5ffdf4f29a
Tanmaya Raj Behera <52962189+tanmaya28@users.noreply.github.com>
2023-11-03 19:15:42 +0530

    Bike Sharing LR.py

--- Bike Sharing LR.py
+++ Bike Sharing LR.py
@@ -1,15 +1,5 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import warnings
 warnings.filterwarnings('ignore')
-
-
-# In[2]:
-
 
 import numpy as np
 import pandas as pd
@@ -26,72 +16,27 @@ from sklearn.metrics import r2_score, mean_squared_error
 
 import statsmodels.api as sm
 
-
-# In[3]:
-
-
 df = pd.read_csv("F:\Projects & Assignments\day.csv")
 df.head()
 
-
-# In[4]:
-
-
 df.shape
-
-
-# In[5]:
-
 
 df.info()
 
-
-# In[6]:
-
-
 df.duplicated().sum()
-
-
-# In[7]:
-
 
 df.drop(['instant', 'dteday','casual','registered','atemp'], axis=1, inplace=True)
 
-
-# In[8]:
-
-
 df.shape
-
-
-# In[9]:
-
 
 cat_vars = ['season','yr','mnth','holiday','weekday', 'workingday','weathersit']
 num_vars = ['temp', 'hum','windspeed','cnt']
 
-
-# In[10]:
-
-
 df[cat_vars] = df[cat_vars].astype('category')
-
-
-# In[11]:
-
 
 df.describe()
 
-
-# In[12]:
-
-
 df.describe(include=['category'])
-
-
-# In[13]:
-
-
 df['season'] = df['season'].map({1:'spring', 2:'summer', 3:'fall', 4:'winter'})
 
 df['weekday'] = df['weekday'].map({0: 'Sun', 1: 'Mon', 2: 'Tue', 3: 'Wed', 4: 'Thu', 5: 'Fri', 6: 'Sat'})
@@ -100,23 +45,11 @@ df['mnth'] = df['mnth'].map({1:'jan', 2:'feb', 3:'mar', 4:'apr', 5: 'may', 6: 'j
                              11: 'nov', 12:'dec'})
 df['weathersit'] = df['weathersit'].map({1: 'Clear_FewClouds', 2: 'Mist_Cloudy', 3: 'LightSnow_LightRain', 4: 'HeavyRain_IcePallets'})
 
-
-# In[14]:
-
-
 df.info()
-
-
-# In[15]:
-
 
 plt.figure(figsize=(20,5))
 plt.plot(df.cnt)
 plt.show()
-
-
-# In[16]:
-
 
 var = df.select_dtypes(exclude = 'category').columns
 col = 2
@@ -130,10 +63,6 @@ for i in list(enumerate(var)):
 plt.tight_layout()   
 plt.show()
 
-
-# In[17]:
-
-
 def percentage_outlier(x):
     iqr = df[x].quantile(0.75)-df[x].quantile(0.25)
     HL = df[x].quantile(0.75)+iqr*1.5
@@ -144,9 +73,6 @@ def percentage_outlier(x):
 
 print('Percentage of outlier (hum): ', percentage_outlier('hum'))
 print('Percentage of outlier (windspeed): ', percentage_outlier('windspeed'))
-
-
-# In[18]:
 
 
 df_piplot=df.select_dtypes(include='category')
@@ -162,22 +88,11 @@ for i in range(1,df_piplot.shape[1]+1):
 plt.show()
 
 
-# In[19]:
-
-
 sns.pairplot(df.select_dtypes(['int64','float64']), diag_kind='kde')
 plt.show()
 
-
-# In[20]:
-
-
 sns.heatmap(df.corr(), annot=True, cmap='coolwarm')
 plt.show()
-
-
-# In[21]:
-
 
 col = 3
 row = len(cat_vars)//col+1
@@ -191,9 +106,6 @@ plt.tight_layout(pad = 1)
 plt.show()
 
 
-# In[22]:
-
-
 dummy_vars = pd.get_dummies(df[['season','weekday','mnth','weathersit']],drop_first=True)
 
 df = pd.concat([df,dummy_vars], axis = 1)
@@ -203,86 +115,39 @@ df.drop(['season','weekday','mnth','weathersit'], axis=1, inplace=True)
 df.head()
 
 
-# In[23]:
-
-
 df.shape
 
-
-# In[25]:
-
-
 df.info()
-
-
-# In[26]:
-
 
 df[['yr','holiday','workingday']]= df[['yr','holiday','workingday']].astype('uint8')
 df.info()
 
-
-# In[28]:
-
-
 df[['yr','holiday','workingday']]= df[['yr','holiday','workingday']].astype('uint8')
 df.info()
-
-
-# In[29]:
-
 
 df_train, df_test = train_test_split(df, train_size = 0.7, random_state = 10 )
 print(df_train.shape)
 print(df_test.shape)
 
-
-# In[30]:
-
-
 scaler = MinMaxScaler()
 df_train[num_vars] = scaler.fit_transform(df_train[num_vars])
 df_train.head()
 
-
-# In[31]:
-
-
 df_test.head()
-
-
-# In[32]:
-
 
 df_test[num_vars] = scaler.transform(df_test[num_vars])
 df_test.head()
 
-
-# In[33]:
-
-
 y_train = df_train.pop('cnt')
 X_train = df_train
 X_train.head()
-
-
-# In[34]:
-
 
 y_test = df_test.pop('cnt')
 X_test = df_test
 
 X_test.head()
 
-
-# In[35]:
-
-
 X_train.columns
-
-
-# In[37]:
-
 
 lm = LinearRegression()
 lm.fit(X_train, y_train)
@@ -292,13 +157,7 @@ col = X_train.columns[rfe.support_]
 col
 
 
-# In[38]:
-
-
 X_train_rfe = X_train[col]
-
-
-# In[39]:
 
 
 def sm_linearmodel(X_train_sm):
@@ -306,9 +165,6 @@ def sm_linearmodel(X_train_sm):
 
    lm = sm.OLS(y_train,X_train_sm).fit()
    return lm
-
-
-# In[40]:
 
 
 def vif_calc(X):
@@ -319,16 +175,9 @@ def vif_calc(X):
     vif = vif.sort_values(by='VIF', ascending = False)
     return vif
 
-
-# In[41]:
-
-
 lm_1 = sm_linearmodel(X_train_rfe)
 print(lm_1.summary())
 print(vif_calc(X_train_rfe))
-
-
-# In[42]:
 
 
 pvalue = lm_1.pvalues
@@ -341,16 +190,9 @@ while(max(pvalue)>0.05):
     pvalue = lm_1.pvalues
 
 
-# In[43]:
-
-
 print(lm_1.summary())
 
 print(vif_calc(X_train_rfe))
-
-
-# In[44]:
-
 
 X_train_new = X_train_rfe.drop(['hum'],axis = 1)
 
@@ -358,17 +200,10 @@ lm_2 = sm_linearmodel(X_train_new)
 print(lm_2.summary())
 print(vif_calc(X_train_new))
 
-
-# In[45]:
-
-
 X_train_new = X_train_new.drop(['season_fall'],axis = 1)
 lm_3 = sm_linearmodel(X_train_new)
 print(lm_3.summary())
 print(vif_calc(X_train_new))
-
-
-# In[46]:
 
 
 X_train_new = X_train_new.drop(['mnth_mar'],axis = 1)
@@ -376,18 +211,10 @@ lm_4 = sm_linearmodel(X_train_new)
 print(lm_4.summary())
 print(vif_calc(X_train_new))
 
-
-# In[47]:
-
-
 X_train_new = X_train_new.drop(['mnth_oct'],axis = 1)
 lm_5 = sm_linearmodel(X_train_new)
 print(lm_5.summary())
 print(vif_calc(X_train_new))
-
-
-# In[48]:
-
 
 lm_final = lm_5
 var_final = list(lm_final.params.index)
@@ -396,31 +223,15 @@ print('Final Selected Variables:', var_final)
 print('\033[1m{:10s}\033[0m'.format('\nCoefficent for the variables are:'))
 print(round(lm_final.params,3))
 
-
-# In[49]:
-
-
 X_train_res = X_train[var_final]
-
-
-# In[50]:
-
 
 X_train_res = sm.add_constant(X_train_res)
 y_train_pred = lm_final.predict(X_train_res)
-
-
-# In[51]:
-
 
 res = y_train - y_train_pred
 sns.distplot(res)
 plt.title('Error terms')
 plt.show()
-
-
-# In[52]:
-
 
 c = [i for i in range(1,len(y_train)+1,1)]
 fig = plt.figure(figsize=(8,5))
@@ -429,42 +240,19 @@ fig.suptitle('Error Terms', fontsize=16)
 plt.xlabel('Y_train_pred', fontsize=14)
 plt.ylabel('Residual', fontsize=14)  
 
-
-# In[53]:
-
-
 df_test.head()
-
-
-# In[54]:
-
 
 X_test_sm = X_test[var_final]
 X_test_sm.head()
 
-
-# In[57]:
-
-
 X_test_sm = sm.add_constant(X_test_sm)
 X_test_sm.head()
-
-
-# In[58]:
-
 
 y_test_pred = lm_final.predict(X_test_sm)
 
 
-# In[59]:
-
-
 r2_test = r2_score(y_true = y_test, y_pred = y_test_pred)
 print('R-Squared for Test dataset: ', round(r2_test,3))
-
-
-# In[60]:
-
 
 N= len(X_test)
 p =len(var_final)
@@ -472,24 +260,14 @@ r2_test_adj = round((1-((1-r2_test)*(N-1)/(N-p-1))),3)
 print('Adj. R-Squared for Test dataset: ', round(r2_test_adj,3))
 
 
-# In[61]:
-
-
 mse = mean_squared_error(y_test, y_test_pred)
 print('Mean_Squared_Error :' ,round(mse,4))
-
-
-# In[62]:
 
 
 res_test = y_test - y_test_pred
 plt.title('Error Terms', fontsize=16) 
 sns.distplot(res_test)
 plt.show()
-
-
-# In[63]:
-
 
 c = [i for i in range(1,len(y_test)+1,1)]
 fig = plt.figure(figsize=(8,5))
@@ -498,28 +276,16 @@ fig.suptitle('Error Terms', fontsize=16)
 plt.xlabel('Y_test_pred', fontsize=14)
 plt.ylabel('Residual', fontsize=14)   
 
-
-# In[64]:
-
-
 fig = plt.figure()
 plt.scatter(y_test, y_test_pred)
 fig.suptitle('y_test vs y_pred', fontsize = 20)
 plt.xlabel('y_test', fontsize = 18)
 plt.ylabel('y_test_pred', fontsize = 16) 
 
-
-# In[65]:
-
-
 print('R- Sqaured train: ', round(lm_final.rsquared,2), '  Adj. R-Squared train:', round(lm_final.rsquared_adj,3) )
 print('R- Sqaured test : ', round(r2_test,2), '  Adj. R-Squared test :', round(r2_test_adj,3))
 print('\033[1m{:10s}\033[0m'.format('\nCoefficent for the variables are:'))
 print(round(lm_final.params,3))
-
-
-# In[ ]:
-
 
 
 
